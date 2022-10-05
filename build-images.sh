9111881dcc0ba3fd0791a31fc54eb76c167c25e1
Robert J. Berger <rob.berger@informed.iq>
2022-10-05 11:58:25 -0700

    Working

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 3.8/Dockerfile -t cimg/python:3.8.13 -t cimg/python:3.8 .
-docker build --file 3.8/node/Dockerfile -t cimg/python:3.8.13-node -t cimg/python:3.8-node .
-docker build --file 3.8/browsers/Dockerfile -t cimg/python:3.8.13-browsers -t cimg/python:3.8-browsers .
+docker build --file 3.8/Dockerfile -t 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8.13 -t 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8 .
+docker build --file 3.8/node/Dockerfile -t 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8.13-node -t 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8-node .
+docker build --file 3.8/browsers/Dockerfile -t 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8.13-browsers -t 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/python:3.8
-docker push cimg/python:3.8.13
-docker push cimg/python:3.8-node
-docker push cimg/python:3.8.13-node
-docker push cimg/python:3.8-browsers
-docker push cimg/python:3.8.13-browsers
+docker push 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8
+docker push 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8.13
+docker push 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8-node
+docker push 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8.13-node
+docker push 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8-browsers
+docker push 456773209430.dkr.ecr.us-west-2.amazonaws.com/python:3.8.13-browsers
